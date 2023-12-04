1dec7b0d47fbe418ad93c6d0db4f73345c33dd32
Thibault MONEGIER <thibault.monegierdusorbier@ext.bpifrance.fr>
2023-12-04 23:19:16 +0100

    feat: update day2 solution

--- src/main/java/org/example/days/Day2.java
+++ src/main/java/org/example/days/Day2.java
@@ -3,6 +3,7 @@ package org.example.days;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day2 {
@@ -11,31 +12,48 @@ public class Day2 {
     public static final String BLUE = "blue";
     public static final String RED = "red";
     public static final String GREEN = "green";
+    public static final Pattern PATTERN_FOR_GAME_ID = Pattern.compile("Game (?<gameId>\\d*):*");
+    public static final Pattern PATTERN_FOR_COLOR = Pattern.compile("(\\d*) (?<color>blue|red|green)");
 
-    public static long solvePart2(Path path) throws IOException {
-        var patternForColor = Pattern.compile("(\\d*) (?<color>blue|red|green)");
+    public static long solve(Path path, boolean isPart2) throws IOException {
+        boolean isPart1 = !isPart2;
+        return Files.lines(path).map(
+            line -> {
+                var matcherForGameId = PATTERN_FOR_GAME_ID.matcher(line);
 
-        int sumsOfPower = 0;
+                if(matcherForGameId.find()) {
+                    int currentColor, minRed =0, minBlue =0, minGreen =0;
+                    var matcherForColors = PATTERN_FOR_COLOR.matcher(line);
 
-        for (String line : Files.readAllLines(path)) {
-            int currentColor = 0, minRed =0, minBlue =0, minGreen =0;
-            var matcherForColor = patternForColor.matcher(line);
-
-            while(matcherForColor.find()) {
-                currentColor = Integer.parseInt(matcherForColor.group(1));
-                if(matcherForColor.group(COLOR).equals(BLUE) && currentColor > minBlue) {
-                    minBlue = currentColor;
+                    while(matcherForColors.find()) {
+                        currentColor = Integer.parseInt(matcherForColors.group(1));
+                        if( isPart1 && isColorAboveLimits(matcherForColors, currentColor)
+                        ) {
+                            return 0;
+                        }
+                        if(matcherForColors.group(COLOR).equals(BLUE) && currentColor > minBlue) {
+                            minBlue = currentColor;
+                        }
+                        if(matcherForColors.group(COLOR).equals(RED) && currentColor > minRed) {
+                            minRed = currentColor;
+                        }
+                        if(matcherForColors.group(COLOR).equals(GREEN) && currentColor > minGreen) {
+                            minGreen = currentColor;
+                        }
+                    }
+                    if(isPart1) {
+                        return Integer.parseInt(matcherForGameId.group("gameId"));
+                    }
+                    return minRed*minBlue*minGreen;
                 }
-                if(matcherForColor.group(COLOR).equals(RED) && currentColor > minRed) {
-                    minRed = currentColor;
-                }
-                if(matcherForColor.group(COLOR).equals(GREEN) && currentColor > minGreen) {
-                    minGreen = currentColor;
-                }
+                return 0;
             }
-            sumsOfPower += minRed*minBlue*minGreen;
-        }
+        ).reduce(0, Integer::sum);
+    }
 
-        return sumsOfPower;
+    private static boolean isColorAboveLimits(Matcher matcherForColors, int currentColor) {
+        return (matcherForColors.group(COLOR).equals(BLUE) && currentColor > 14) ||
+                (matcherForColors.group(COLOR).equals(RED) && currentColor > 12) ||
+                (matcherForColors.group(COLOR).equals(GREEN) && currentColor > 13);
     }
 }
