eebe9c4e129f86f14b9da5e505be5bcccd01387a
Thibault MONEGIER <thibault.monegierdusorbier@ext.bpifrance.fr>
2023-12-09 22:37:52 +0100

    feat: update day2 solution

--- src/main/java/org/example/days/Day2.java
+++ src/main/java/org/example/days/Day2.java
@@ -3,57 +3,54 @@ package org.example.days;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Day2 {
-
-    public static final String COLOR = "color";
-    public static final String BLUE = "blue";
-    public static final String RED = "red";
-    public static final String GREEN = "green";
-    public static final Pattern PATTERN_FOR_GAME_ID = Pattern.compile("Game (?<gameId>\\d*):*");
-    public static final Pattern PATTERN_FOR_COLOR = Pattern.compile("(\\d*) (?<color>blue|red|green)");
+    private static final String COLOR = "color", BLUE = "blue", RED = "red", GREEN = "green";
+    private static final Map<String, Integer> maxima = Map.of(
+        BLUE, 14, RED, 12, GREEN, 13
+    );
+    private static final Pattern
+            PATTERN_FOR_GAME_ID = Pattern.compile("Game (?<gameId>\\d*):*"),
+            PATTERN_FOR_COLOR = Pattern.compile("(?<numberOfCubes>\\d*) (?<color>blue|red|green)");
 
     public static long solve(Path path, boolean isPart2) throws IOException {
-        boolean isPart1 = !isPart2;
         return Files.lines(path).map(
             line -> {
                 var matcherForGameId = PATTERN_FOR_GAME_ID.matcher(line);
-
-                if(matcherForGameId.find()) {
-                    int currentColor, minRed =0, minBlue =0, minGreen =0;
-                    var matcherForColors = PATTERN_FOR_COLOR.matcher(line);
-
-                    while(matcherForColors.find()) {
-                        currentColor = Integer.parseInt(matcherForColors.group(1));
-                        if( isPart1 && isColorAboveLimits(matcherForColors, currentColor)
-                        ) {
-                            return 0;
-                        }
-                        if(matcherForColors.group(COLOR).equals(BLUE) && currentColor > minBlue) {
-                            minBlue = currentColor;
-                        }
-                        if(matcherForColors.group(COLOR).equals(RED) && currentColor > minRed) {
-                            minRed = currentColor;
-                        }
-                        if(matcherForColors.group(COLOR).equals(GREEN) && currentColor > minGreen) {
-                            minGreen = currentColor;
-                        }
-                    }
-                    if(isPart1) {
-                        return Integer.parseInt(matcherForGameId.group("gameId"));
-                    }
-                    return minRed*minBlue*minGreen;
+                if(!matcherForGameId.find()) {
+                    return 0;
                 }
-                return 0;
+                return calculateLineResult(line, !isPart2, Integer.parseInt(matcherForGameId.group("gameId")));
             }
         ).reduce(0, Integer::sum);
     }
 
-    private static boolean isColorAboveLimits(Matcher matcherForColors, int currentColor) {
-        return (matcherForColors.group(COLOR).equals(BLUE) && currentColor > 14) ||
-                (matcherForColors.group(COLOR).equals(RED) && currentColor > 12) ||
-                (matcherForColors.group(COLOR).equals(GREEN) && currentColor > 13);
+    private static int calculateLineResult(String line, boolean isPart1, Integer gameId) {
+        var matcherForColors = PATTERN_FOR_COLOR.matcher(line);
+        var minima = new HashMap<String, Integer>(){{
+            this.put(BLUE, 0);
+            this.put(RED, 0);
+            this.put(GREEN, 0);
+        }};
+
+        while(matcherForColors.find()) {
+            int numberOfCubes = Integer.parseInt(matcherForColors.group("numberOfCubes"));
+            String colorType = matcherForColors.group(COLOR);
+            if(isColorAboveLimits(colorType,numberOfCubes, isPart1)) {
+                return 0;
+            }
+            minima.put(colorType, Math.max(numberOfCubes, minima.get(colorType)));
+        }
+        if(isPart1) {
+            return gameId;
+        }
+        return minima.values().stream().reduce(1, (a, b) -> a * b);
+    }
+
+    private static boolean isColorAboveLimits(String colorType, int currentColor, boolean isPart1) {
+        return isPart1 && currentColor > maxima.get(colorType);
     }
 }
