d530f53c8f526e7703d2d1aadc9bed38c9c4bf29
Thibault MONEGIER <thibault.monegierdusorbier@ext.bpifrance.fr>
2023-12-10 00:33:15 +0100

    feat: update day6 solution

--- src/main/java/org/example/days/Day6.java
+++ src/main/java/org/example/days/Day6.java
@@ -3,9 +3,11 @@ package org.example.days;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static java.util.function.Predicate.not;
 
@@ -13,42 +15,47 @@ public class Day6 {
     public static final Pattern PATTERN_FOR_TIME = Pattern.compile("Time:(?<times>.*)");
     public static final Pattern PATTERN_FOR_DISTANCE = Pattern.compile("Distance: (?<recordDistances>.*)");
 
+    public static long solve(Path filePath, boolean isPart2) throws IOException {
+        List<String> lines = Files.readAllLines(filePath);
+        var matcherForTime = PATTERN_FOR_TIME.matcher(lines.get(0));
+        var matcherForDistance = PATTERN_FOR_DISTANCE.matcher(lines.get(1));
+        if(!matcherForTime.find() || !matcherForDistance.find()){
+            return 0;
+        }
+        ListRaces listRaces = retrieveRaces(matcherForTime, matcherForDistance, !isPart2);
+        return IntStream.range(0, listRaces.times.size())
+                .boxed()
+                .map(listRaces::getRace)
+                .map(Race::numberOfWaysOfWinning)
+                .reduce(1L, (a, b) -> a*b);
+    }
+
+    private static ListRaces retrieveRaces(Matcher matcherForTime, Matcher matcherForDistance, boolean isPart1) {
+        final String times = matcherForTime.group("times");
+        final String recordDistances = matcherForDistance.group("recordDistances");
+        if(isPart1) {
+            return new ListRaces(
+                Arrays.stream(times.split(" ")).filter(not((String::isBlank))).map(Long::parseLong).toList(),
+                Arrays.stream(recordDistances.split(" ")).filter(not((String::isBlank))).map(Long::parseLong).toList()
+            );
+        }
+        return new ListRaces(
+            List.of(Long.parseLong(times.replace(" ", ""))),
+            List.of(Long.parseLong(recordDistances.replace(" ", "")))
+        );
+    }
+
     private record Race(long time, long recordDistance){
         long numberOfWaysOfWinning() {
-            int count = 0;
-            for(int i = 1; i< time; i++) {
-                if(i * (time - i) > recordDistance) {
-                    count++;
-                }
-            }
-            return count;
+            return LongStream.range(1, time).filter(
+                i -> i * (time - i) > recordDistance
+            ).count();
         }
     }
 
-    public static long solve(Path filePath, boolean isPart2) throws IOException {
-        List<String> lines = Files.readAllLines(filePath);
-        var matcherFortTime = PATTERN_FOR_TIME.matcher(lines.get(0));
-        var matcherForDistance = PATTERN_FOR_DISTANCE.matcher(lines.get(1));
-        if(!matcherFortTime.find() || !matcherForDistance.find()){
-            return 0;
+    private record ListRaces(List<Long> times, List<Long> recordDistances) {
+        Race getRace(int raceIndex) {
+            return new Race(times.get(raceIndex), recordDistances.get(raceIndex));
         }
-
-        List<Long> times, recordDistance;
-
-        if(!isPart2) {
-            times = Arrays.stream(matcherFortTime.group("times").split(" ")).filter(not((String::isBlank))).map(Long::parseLong).toList() ;
-            recordDistance = Arrays.stream(matcherForDistance.group("recordDistances").split(" ")).filter(not((String::isBlank))).map(Long::parseLong).toList() ;
-        } else {
-            times = List.of(Long.parseLong(matcherFortTime.group("times").replace(" ", "")));
-            recordDistance = List.of(Long.parseLong(matcherForDistance.group("recordDistances").replace(" ", "")));
-        }
-
-        assert times.size() == recordDistance.size();
-        return IntStream.range(0, times.size())
-                .boxed()
-                .map(
-                    i -> new Race(times.get(i), recordDistance.get(i)).numberOfWaysOfWinning()
-                )
-                .reduce(1L, (a, b) -> a*b);
     }
 }
