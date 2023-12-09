3b5e5bd8ddac8dbaebce3fe2e75b00a81b7acde0
Thibault MONEGIER <thibault.monegierdusorbier@ext.bpifrance.fr>
2023-12-09 23:42:40 +0100

    feat: update day4 solution

--- src/main/java/org/example/days/Day4.java
+++ src/main/java/org/example/days/Day4.java
@@ -6,8 +6,10 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
@@ -17,42 +19,51 @@ public class Day4 {
     public static final Pattern PATTERN_FOR_LINE = Pattern.compile(".*: (?<winning>.*)\\|(?<actual>.*)");
 
     public static long solve(Path filePath, boolean isPart2) throws IOException {
-        int res = 0;
-        boolean isPart1 = !isPart2;
-
         List<String> lines = Files.readAllLines(filePath);
         int[] numberOfCopies = new int[lines.size()];
 
-        for (int lineIndex = 0; lineIndex < lines.size(); lineIndex++) {
-            String line = lines.get(lineIndex);
-            var matcher = PATTERN_FOR_LINE.matcher(line);
-
-            if(matcher.find()) {
-                Set<Integer> winningNumbers = Arrays.stream(matcher.group("winning").split(" "))
-                        .filter(not(String::isEmpty))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toSet());
-
-                var streamOfActualWinningNumber =  Arrays.stream(matcher.group("actual").split(" "))
-                        .filter(not(String::isEmpty))
-                        .map(Integer::parseInt)
-                        .filter(winningNumbers::contains);
-
-                if(isPart1) {
-                    res += calculateScoreOfCurrentCard(streamOfActualWinningNumber);
-                } else {
-                    long numberOfActualNumbersWinning = streamOfActualWinningNumber.count();
-                    do {
-                        res++;
-                        for(int j = lineIndex+1; j <= lineIndex+numberOfActualNumbersWinning; j++) {
-                            numberOfCopies[j]++;
-                        }
-                        numberOfCopies[lineIndex] --;
-                    } while (numberOfCopies[lineIndex] >= 0);
+        return IntStream.range(0, lines.size())
+            .map(
+                lineIndex -> {
+                    String line = lines.get(lineIndex);
+                    var matcher = PATTERN_FOR_LINE.matcher(line);
+                    if(!matcher.find()) {
+                        return 0;
+                    }
+                    return calculateResult(lineIndex, matcher, !isPart2, numberOfCopies);
                 }
-            }
+            )
+            .reduce(0, Integer::sum);
+    }
+
+    private static Integer calculateResult(Integer lineIndex, Matcher matcher, boolean isPart1, int[] numberOfCopies) {
+        Set<Integer> winningNumbers = retrieveWinningNumbers(matcher);
+        var actualWinningNumbers = retrieveActualWinningNumbers(matcher, winningNumbers);
+        if(isPart1) {
+            return calculateScoreOfCurrentCard(actualWinningNumbers);
         }
-        return res;
+        processWinningCard(lineIndex, numberOfCopies, actualWinningNumbers.count());
+        return numberOfCopies[lineIndex]+1;
+    }
+
+    private static Stream<Integer> retrieveActualWinningNumbers(Matcher matcher, Set<Integer> winningNumbers) {
+        return Arrays.stream(matcher.group("actual").split(" "))
+                .filter(not(String::isEmpty))
+                .map(Integer::parseInt)
+                .filter(winningNumbers::contains);
+    }
+
+    private static Set<Integer> retrieveWinningNumbers(Matcher matcher) {
+        return Arrays.stream(matcher.group("winning").split(" "))
+                .filter(not(String::isEmpty))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
+
+    private static void processWinningCard(Integer lineIndex, int[] numberOfCopies, long numberOfActualNumbersWinning) {
+        for(int j = 1; j <= numberOfActualNumbersWinning; j++) {
+            numberOfCopies[lineIndex + j] += numberOfCopies[lineIndex]+1;
+        }
     }
 
     private static Integer calculateScoreOfCurrentCard(Stream<Integer> streamOfActualWinningNumber) {
